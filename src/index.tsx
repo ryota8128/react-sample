e88f9fea6dd6c112711f92dbb0376be9a05877cd
ryota8128 <ryota2339@icloud.com>
2023-06-10 14:19:14 +0900

    components追加

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
+import Name from './components/Name';
+import Parent from './components/ContainerSample';
+import Message from './components/Message';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <App /> */}
+        <Hello />
+        <Name />
+        <Parent />
+        <Message />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
